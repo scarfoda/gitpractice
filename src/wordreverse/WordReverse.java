e7a8261d28d8d4f2220b17a24212a02646b58e9a
scarfoda <scarfoda@sheridancollege.ca>
2021-05-26 13:16:20 -0400

    word reverse application first commit

--- src/wordreverse/WordReverse.java
+++ src/wordreverse/WordReverse.java
@@ -15,8 +15,10 @@ public class WordReverse {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        for ();
+        
+        for (int i = 0; i < 2; i++) { 
+            System.out.print("This is fine.");
+        }
     }
     
 }
